79d0ddc101d82d5fd97891d06d13f3888920ba99
钱多多 <qdd@qianduoduodeMacBook-Pro.local>
2017-04-11 00:33:26 +0800

    use simple_format helper to decorate

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,6 +1,12 @@
 class Account::PostsController < ApplicationController
   before_action :authenticate_user!
+
   def index
     @posts = current_user.posts
+  end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post
   end
 end
