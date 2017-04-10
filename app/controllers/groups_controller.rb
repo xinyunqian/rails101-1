6e808302b84f81277e088e2a87c905a2eb79a4ab
钱多多 <qdd@qianduoduodeMacBook-Pro.local>
2017-04-10 15:34:48 +0800

    implement groups#edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,7 +7,11 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
   end
-  
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+    
   def new
     @group = Group.new
   end
